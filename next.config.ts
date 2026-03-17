4a29e2a4029e28026f8e9fb9b4611b3ead8e6f10
Benjamin Huang <benhuang21828@gmail.com>
2026-03-16 22:07:11 -0400

    fix: conditionally set basePath to enable local static exporting

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/oil-bench',
+  basePath: isGithubActions ? '/oil-bench' : '',
 };
 
 export default nextConfig;
